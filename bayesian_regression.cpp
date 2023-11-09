1657b93cb022d2c98d449a00c18eeefca5fbe123
Shantanu Ambekar <shngambekar@gmail.com>
2023-11-09 21:46:04 +0530

    updated code

--- bayesian_regression.cpp
+++ bayesian_regression.cpp
@@ -1,111 +1,101 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
 #include <fstream>
 #include <sstream>
-#define testNum 10
-#define dataSize 10
+#include <string>
 
-void readDataFromCSV(const std::string& filename, double* features, double* labels, int num_samples) {
-    std::ifstream file(filename);
+using namespace std;
 
+struct Point {
+    double x;
+    double y;
+};
+
+double stringToDouble(const string& str) {
+    double result = 0.0;
+    stringstream ss(str);
+    ss >> result;
+    return result;
+}
+
+vector<Point> readCSVFile(const string& filename) {
+    vector<Point> data;
+
+    ifstream file(filename);
     if (!file.is_open()) {
-        std::cerr << "Error opening file: " << filename << std::endl;
-        return;
+        cerr << "Error opening the file." << endl;
+        return data;
     }
 
-    std::string line;
-    int count = 0;
-    while (std::getline(file, line) && count < num_samples) {
-        std::istringstream linestream(line);
-        std::string value;
-        if (std::getline(linestream, value, ',') && !value.empty()) {
-            try {
-                features[count] = std::stod(value);
-            } catch (const std::invalid_argument& e) {
-                std::cerr << "Error converting height data at line " << count + 1 << ": " << e.what() << std::endl;
-                continue;
-            }
-        } else {
-            std::cerr << "Empty or invalid height data at line " << count + 1 << std::endl;
-            continue;
+    string line;
+    while (getline(file, line)) {
+        Point point;
+        stringstream linestream(line);
+        string cell;
+
+        if (getline(linestream, cell, ',')) {
+            point.x = stringToDouble(cell);
         }
 
-        if (std::getline(linestream, value, ',') && !value.empty()) {
-            try {
-                labels[count] = std::stod(value);
-            } catch (const std::invalid_argument& e) {
-                std::cerr << "Error converting weight data at line " << count + 1 << ": " << e.what() << std::endl;
-                continue;
-            }
-        } else {
-            std::cerr << "Empty or invalid weight data at line " << count + 1 << std::endl;
-            continue;
+        if (getline(linestream, cell, ',')) {
+            point.y = stringToDouble(cell);
         }
 
-        count++;
+        data.push_back(point);
     }
 
     file.close();
+    return data;
+}
+
+double likelihood(const vector<Point>& data, double slope, double intercept, double prior_stddev) {
+    double log_likelihood = 0.0;
+    for (const auto& point : data) {
+        double error = point.y - (slope * point.x + intercept);
+        log_likelihood += -(error * error) / (2 * prior_stddev * prior_stddev);
+    }
+    return log_likelihood;
 }
 
 int main() {
-    const int num_samples = dataSize; // Number of data points
+    vector<Point> data = readCSVFile("SOCR-HeightWeight.csv");
 
-    double features[num_samples];
-    double labels[num_samples];
+    // Priors for the slope (m) and intercept (c)
+    double prior_stddev = 1.0; // Standard deviation for the priors
 
-    readDataFromCSV("SOCR-HeightWeight.csv", features, labels, num_samples);
+    const int num_iterations = 10000; // Number of iterations
 
+    double slope = 0.0;
+    double intercept = 0.0;
 
-    double weights[2] = {0}; // Weights: slope and intercept
-    double prior_covariance[2][2] = {{1, 0}, {0, 1}}; // Simple prior covariance matrix
-    double prior_precision = 0.1; // Set the precision for the prior covariance
-    double sigma = 0.1; // Noise parameter (for likelihood)
+    default_random_engine gen;
+    normal_distribution<double> dist(0.0, prior_stddev);
 
-    for (int i = 0; i < num_samples; ++i) {
-        double X[2] = {1, features[i]}; // Add bias term to features
+    for (int iter = 0; iter < num_iterations; ++iter) {
+        double proposed_slope = slope + dist(gen);
+        double proposed_intercept = intercept + dist(gen);
 
-        double prior_mean[2] = {weights[0], weights[1]}; // Use current weights as prior mean
+        double log_likelihood_current = likelihood(data, slope, intercept, prior_stddev);
+        double log_likelihood_proposed = likelihood(data, proposed_slope, proposed_intercept, prior_stddev);
 
-        double X_T_X[2][2] = {
-            {X[0] * X[0], X[0] * X[1]},
-            {X[1] * X[0], X[1] * X[1]}
-        };
+        // Metropolis-Hastings step
+        if (log_likelihood_proposed - log_likelihood_current > log(uniform_real_distribution<double>(0, 1)(gen))) {
+            slope = proposed_slope;
+            intercept = proposed_intercept;
+        }
 
-        double posterior_covariance[2][2] = {
-            {X_T_X[0][0] / sigma + prior_covariance[0][0], X_T_X[0][1] / sigma + prior_covariance[0][1]},
-            {X_T_X[1][0] / sigma + prior_covariance[1][0], X_T_X[1][1] / sigma + prior_covariance[1][1]}
-        };
-
-        double determinant = (posterior_covariance[0][0] * posterior_covariance[1][1]) - (posterior_covariance[0][1] * posterior_covariance[1][0]);
-
-        double posterior_precision[2][2] = {
-            {posterior_covariance[1][1] / determinant, -posterior_covariance[0][1] / determinant},
-            {-posterior_covariance[1][0] / determinant, posterior_covariance[0][0] / determinant}
-        };
-
-        double X_T_labels[2] = {X[0] * labels[i], X[1] * labels[i]};
-
-        weights[0] = weights[0] + (
-            posterior_precision[0][0] * (prior_precision * prior_covariance[0][0] * prior_mean[0] + X_T_labels[0] / sigma) +
-            posterior_precision[0][1] * (prior_precision * prior_covariance[0][1] * prior_mean[1] + X_T_labels[1] / sigma)
-        );
-
-        weights[1] = weights[1] + (
-            posterior_precision[1][0] * (prior_precision * prior_covariance[1][0] * prior_mean[0] + X_T_labels[0] / sigma) +
-            posterior_precision[1][1] * (prior_precision * prior_covariance[1][1] * prior_mean[1] + X_T_labels[1] / sigma)
-        );
-
-        prior_covariance[0][0] = posterior_covariance[0][0];
-        prior_covariance[0][1] = posterior_covariance[0][1];
-        prior_covariance[1][0] = posterior_covariance[1][0];
-        prior_covariance[1][1] = posterior_covariance[1][1];
+        // Output estimated parameters at certain intervals or at the end
+        if ((iter + 1) % 1000 == 0 || iter == num_iterations - 1) {
+            cout << "Iteration " << iter + 1 << ": Slope: " << slope << ", Intercept: " << intercept << endl;
+        }
     }
 
-    // Make predictions using the obtained weights
-    for (int i = 0; i < testNum; ++i) {
-        double predicted_label = weights[1] * features[i] + weights[0]; // Predicted labels
-        std::cout << "Predicted label for feature " << features[i] << ": " << predicted_label << std::endl;
-    }
+    float testInput = 165;
+    float testOutput = testInput * slope + intercept;
+
+    cout << "If height is " << testInput << " weight is " << testOutput << endl;
 
     return 0;
 }
