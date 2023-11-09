1f375296eeb079c5bcf9956a615b68c3c36186b8
DhruvShrimali <95020813+DhruvShrimali@users.noreply.github.com>
2023-11-09 20:29:07 +0530

    Update naive_bayes.cpp

--- naive_bayes.cpp
+++ naive_bayes.cpp
@@ -1,10 +1,9 @@
-#include<bits/stdc++.h>
-// #include <iostream>
-// #include <fstream>
-// #include <sstream>
-// #include <string>
-// #include <vector>
-// #include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
 using namespace std;
 
 class NaiveBayesClassifier {
